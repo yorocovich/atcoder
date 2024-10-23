d522d59d1354094bd1dcf3c23981337470d91cef
yorocovich <roughandpeace11@gmail.com>
2024-10-23 20:58:01 +0900

    20241023

--- atcoder/ABC/ABC335_A.py
+++ atcoder/ABC/ABC335_A.py
@@ -1,4 +1,17 @@
+# A
 s = list(input())
 s[-1] = '4'
 
 print(''.join(s))
+
+
+# B
+n = int(input())
+
+i = j = k = 0
+
+for i in range(n+1):
+  for j in range(n+1):
+    for k in range(n+1):
+      if i+j+k <= n:
+        print(f"{i} {j} {k}")
