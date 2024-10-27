551b27d72e59693b35a788fa49091d733216b88e
yorocovich <roughandpeace11@gmail.com>
2024-10-27 21:45:09 +0900

    20241027 study 2

--- atcoder/ABC/ABC323.py
+++ atcoder/ABC/ABC323.py
@@ -1,3 +1,4 @@
+# A
 s = input()
 
 for i in range(16):
@@ -6,3 +7,17 @@ for i in range(16):
     exit()
 
 print("Yes")
+
+# B
+N = int(input())
+results = []
+for _ in range(N):
+    results.append(input())
+
+wins = []
+for i in range(N):
+    win_count = results[i].count('o')
+    wins.append((win_count, -i, i+1))
+
+wins.sort(reverse=True)
+print(*[player for _, _, player in wins])
