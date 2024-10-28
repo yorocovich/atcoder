d562bce393fa66cfdc4ffdc7c46d21876b833475
yorocovich <roughandpeace11@gmail.com>
2024-10-28 22:45:28 +0900

    20241028 study 2

--- atcoder/ABC/ABC322.py
+++ atcoder/ABC/ABC322.py
@@ -1,3 +1,4 @@
+# A
 n = int(input())
 s = input()
 
@@ -7,3 +8,21 @@ for i in range(2,n):
     exit()
 
 print(-1)
+
+# B
+n, m = map(int,input().split())
+s = input()
+t = input()
+
+end = 0
+if t[0:n] == s:
+  end |= 1
+if t[n*-1:] == s:
+  end |= 2
+
+if end == 3:
+  print(0)
+elif end == 0:
+  print(3)
+else:
+  print(end)
