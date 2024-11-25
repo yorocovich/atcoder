30222e35eaf180ca0c2aceb6c95085b745070a9d
yorocovich <roughandpeace11@gmail.com>
2024-11-25 20:18:28 +0900

    20241125 study 1

--- atcoder/ABC/ABC311.py
+++ atcoder/ABC/ABC311.py
@@ -1,3 +1,4 @@
+# A
 n = int(input())
 s = input()
 chk = 0
@@ -11,3 +12,24 @@ for i in range(n):
   if chk & 7 == 7:
     print(i+1)
     break
+
+# B
+n, d = map(int,input().split())
+strs = []
+for _ in range(n):
+  strs.append(input())
+
+ans = 0
+temp = 0
+for i in range(d):
+  is_free = ""
+  for j in range(n):
+    is_free += strs[j][i]
+  if "x" not in is_free:
+    temp += 1
+  else:
+    ans = max(ans,temp)
+    temp = 0
+
+ans = max(ans,temp)
+print(ans)
