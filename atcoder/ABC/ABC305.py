3459dd9d8001caacad533abbc2e55d01f85b3a41
yorocovich <roughandpeace11@gmail.com>
2024-11-26 06:25:19 +0900

    20241126 study 1

--- atcoder/ABC/ABC305.py
+++ atcoder/ABC/ABC305.py
@@ -1,3 +1,4 @@
+# A
 n = int(input())
 
 x = n // 5
@@ -6,3 +7,13 @@ if n % 5 in (0,1,2):
   print(x * 5)
 else:
   print(x * 5 + 5)
+
+# B
+dis = [0,3,4,8,9,14,23]
+
+p, q = input().split()
+
+dis_p = ord(p.upper()) - ord('A')
+dis_q = ord(q.upper()) - ord('A')
+
+print(abs(dis[dis_p]-dis[dis_q]))
