5fd7e16f825ba3f520b1c21deff068d8c636cbde
yorocovich <roughandpeace11@gmail.com>
2024-12-15 22:54:32 +0900

    20241215 study 1

--- atcoder/ABC/ABC307.py
+++ atcoder/ABC/ABC307.py
@@ -1,3 +1,4 @@
+# A
 n = int(input())
 steps = list(map(int,input().split()))
 ans = []
@@ -10,3 +11,33 @@ for i in range(1,len(steps)+1):
     step = 0
 
 print(*ans)
+
+# B
+def is_palindrome(text: str) -> bool:
+  cleaned_text = "".join(text.lower().split())
+  left = 0
+  right = len(cleaned_text) - 1
+
+  while left < right:
+    if cleaned_text[left] != cleaned_text[right]:
+      return False
+    left += 1
+    right -= 1
+    
+  return True
+
+n = int(input())
+strings = []
+for i in range(n):
+  strings.append(input())
+
+for i in range(len(strings)):
+  for j in range(len(strings)):
+    if i == j:
+      continue
+    else:
+      if is_palindrome(strings[i]+strings[j]):
+        print("Yes")
+        exit()
+
+print("No")
