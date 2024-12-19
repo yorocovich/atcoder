edc213a2891c4011ce80f3cb2b6a930171a1cc0a
yorocovich <roughandpeace11@gmail.com>
2024-12-19 22:33:37 +0900

    20241219 study 1

--- atcoder/ABC/ABC306.py
+++ atcoder/ABC/ABC306.py
@@ -1,8 +1,18 @@
+# A
 n = int(input())
 s = input()
 ans = ""
 
 for i in range(n):
   ans += s[i]*2
+
+print(ans)
+
+# B
+nums = list(map(int,input().split()))
+ans = 0
+
+for i in range(64):
+  ans += nums[i] * (2 ** i)
 
 print(ans)
