6ad567050b1254a53f1bb65e0e51ea869c8e6297
yorocovich <roughandpeace11@gmail.com>
2024-12-23 21:29:51 +0900

    20241223 study 1

--- atcoder/ABC/ABC291.py
+++ atcoder/ABC/ABC291.py
@@ -6,21 +6,11 @@ for i in range(len(s)):
     print(i+1)
 
 # B
-k = int(input())
-calls = list(map(int, input().split()))
-called = set()  # 高速な集合を使用
-ans = []
+n = int(input())
+points = list(map(int,input().split()))
 
-# callsの番号がcalledに含まれていない場合、追加する
-for i in range(k):
-    if i + 1 not in called:  # ここを残します
-        called.add(calls[i])
+points.sort()
+points = points[n:-n]
+ans = sum(points) / len(points)
 
-# 1からkまでのうち、called に含まれていないものを探す
-for j in range(1, k + 1):
-    if j not in called:
-        ans.append(j)
-
-# 結果を出力
-print(len(ans))
-print(*ans)
+print(ans)
