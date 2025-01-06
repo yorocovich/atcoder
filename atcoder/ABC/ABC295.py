386669f7f17f09269a6911c2cb3854227336fe99
yorocovich <roughandpeace11@gmail.com>
2025-01-06 23:38:15 +0900

    20250106 study 1

--- atcoder/ABC/ABC295.py
+++ atcoder/ABC/ABC295.py
@@ -1,3 +1,4 @@
+# A
 n = int(input())
 words = list(input().split())
 
@@ -7,3 +8,22 @@ for word in words:
     exit()
 
 print("No")
+
+# B
+r, c = map(int,input().split())
+map_ori = []
+for i in range(r):
+  map_ori.append(list(input()))
+
+for i in range(r):
+  for j in range(c):
+    if map_ori[i][j] not in (".","#"):
+      L = int(map_ori[i][j])
+      map_ori[i][j] = "."
+      for k in range(r):
+        for l in range(c):
+          if (abs(i - k) + abs(l - j) <= L) and (not map_ori[k][l].isdigit()):
+            map_ori[k][l] = "."
+
+for x in map_ori:
+  print("".join(x))
