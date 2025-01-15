7a5f3458dfc7cc34b788d8c03288084852c824b5
yorocovich <roughandpeace11@gmail.com>
2025-01-15 23:26:59 +0900

    20250115 study

--- atcoder/ABC/ABC288.py
+++ atcoder/ABC/ABC288.py
@@ -1,4 +1,15 @@
+# A
 n = int(input())
 for i in range(n):
   a, b = map(int,input().split())
   print(a+b)
+
+# B
+n, k = map(int,input().split())
+names = []
+for i in range(k):
+  names.append(input())
+
+names.sort()
+
+print(*names, sep="\n")
