2b3429fe49ec57a0a54afa56494020de14979b2d
yorocovich <roughandpeace11@gmail.com>
2025-01-16 22:41:23 +0900

    20250116 study

--- atcoder/ABC/ABC284.py
+++ atcoder/ABC/ABC284.py
@@ -1,3 +1,4 @@
+# A
 n = int(input())
 x = []
 for i in range(n):
@@ -7,3 +8,15 @@ x.reverse()
 
 for s in x:
   print(s)
+
+# B
+test_cases = int(input())
+
+for i in range(test_cases):
+  number_count = int(input())
+  numbers = list(map(int,input().split()))
+  ans = 0
+  for j in range(number_count):
+    if numbers[j] % 2 == 1:
+      ans += 1
+  print(ans)
