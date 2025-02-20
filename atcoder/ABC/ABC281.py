c87e42c5ebdb6ec1a07bdf6aa3ec9a73a5e5b1e1
yorocovich <roughandpeace11@gmail.com>
2025-02-20 23:17:34 +0900

    20250220 study

--- atcoder/ABC/ABC281.py
+++ atcoder/ABC/ABC281.py
@@ -1,4 +1,20 @@
+# A
 n = int(input())
 while n >= 0:
   print(n)
   n -= 1
+
+# B
+s = input()
+if len(s) != 8:
+  print("No")
+  exit()
+
+if not s[1:7].isdigit():
+  print("No")
+  exit()
+  
+if s[0].isalpha() and s[0].isupper() and s[7].isalpha() and s[7].isupper() and 100000 <= int(s[1:7]) <= 999999:
+  print("Yes")
+else:
+  print("No")
