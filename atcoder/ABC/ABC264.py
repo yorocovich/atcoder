d14536e64a25c604c48b5bf930af82772930e21f
yorocovich <roughandpeace11@gmail.com>
2025-02-26 22:31:28 +0900

    20250226 study

--- atcoder/ABC/ABC264.py
+++ atcoder/ABC/ABC264.py
@@ -1,4 +1,13 @@
+# A
 s = "atcoder"
 l, r = map(int,input().split())
 
 print(s[l-1:r])
+
+# B
+r, c = map(int,input().split())
+
+if max(abs(r-8), abs(c-8)) % 2 == 1:
+  print("black")
+else:
+  print("white")
